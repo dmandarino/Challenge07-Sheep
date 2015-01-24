98ed1629cdab3845ec9efa95b7f76e0f65a943c6
dmandarino <douglasmandarino@Douglass-MacBook-Pro.local>
2015-01-24 02:20:15 -0200

    refatorando

--- Challenge07-Sheep/HighScoreScene.m
+++ Challenge07-Sheep/HighScoreScene.m
@@ -13,31 +13,26 @@
 SKLabelNode *scoreLabel;
 
 -(void) didMoveToView:(SKView *)view {
-    SKSpriteNode *bgImage = [SKSpriteNode spriteNodeWithImageNamed:@"background1.png"];
-    bgImage.size = CGSizeMake(self.size.height, self.size.width);
-    bgImage.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
-    [self addChild:bgImage];
+    [self createBackground];
     
+    [self showScore];
     
-    SKLabelNode *titleLabel;
-    titleLabel= [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    titleLabel.fontSize = 30;
-    titleLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame)+30);
-    titleLabel.text = @"Score";
+    SKLabelNode *titleLabel = [self createTitleLabel];
     [self addChild:titleLabel];
     
-    scoreLabel= [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    scoreLabel.fontSize = 30;
-    scoreLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
-    [self addChild:scoreLabel];
-
-    SKSpriteNode *backButtonNode = [SKSpriteNode spriteNodeWithImageNamed:@"retryButton.png"];
-    backButtonNode.position = CGPointMake(CGRectGetMinX(self.frame)+60, CGRectGetMidY(self.frame)-60);
-    backButtonNode.name = @"backButtonNode";//how the node is identified later
-    backButtonNode.zPosition = 1.0;
-    backButtonNode.xScale = 0.4;
-    backButtonNode.yScale = 0.4;
+    SKSpriteNode *backButtonNode = [self createBackButton];
     [self addChild: backButtonNode];
+    
+    
+//    [self saveScore];
+}
+
+-(void) saveScore {
+    NSString *filePath = [[NSBundle mainBundle] pathForResource:@"score" ofType:@"plist"];
+    NSMutableDictionary *plistdict = [NSMutableDictionary dictionaryWithContentsOfFile:filePath];
+    
+    [plistdict writeToFile:filePath atomically:YES];
+
 }
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
@@ -54,6 +49,41 @@ SKLabelNode *scoreLabel;
         scene.scaleMode = SKSceneScaleModeAspectFill;
         [self.view presentScene:scene transition:reveal];
     }
+}
+
+-(SKSpriteNode *) createBackButton {
+    SKSpriteNode *backButtonNode = [SKSpriteNode spriteNodeWithImageNamed:@"retryButton.png"];
+    backButtonNode.position = CGPointMake(CGRectGetMinX(self.frame)+60, CGRectGetMidY(self.frame)-60);
+    backButtonNode.name = @"backButtonNode";//how the node is identified later
+    backButtonNode.zPosition = 1.0;
+    backButtonNode.xScale = 0.4;
+    backButtonNode.yScale = 0.4;
+    
+    return backButtonNode;
+}
+
+-(void) showScore {
+    scoreLabel= [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    scoreLabel.fontSize = 30;
+    scoreLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
+    [self addChild:scoreLabel];
+}
+
+-(SKLabelNode *) createTitleLabel {
+    SKLabelNode *titleLabel ;
+    titleLabel= [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
+    titleLabel.fontSize = 30;
+    titleLabel.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame)+30);
+    titleLabel.text = @"Score";
+    
+    return titleLabel;
+}
+
+-(void) createBackground {
+    SKSpriteNode *bgImage = [SKSpriteNode spriteNodeWithImageNamed:@"background1.png"];
+    bgImage.size = CGSizeMake(self.size.height, self.size.width);
+    bgImage.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+    [self addChild:bgImage];
 }
 
 @end
